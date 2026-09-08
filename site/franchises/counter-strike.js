99af88e368f1d439237ecf47783cb0832bae9bcb
GustavoHSCruz <gustavo08cruz@gmail.com>
2026-09-08 13:57:33 -0300

    Substitui minimapa por HUD da C4

--- site/franchises/counter-strike.js
+++ site/franchises/counter-strike.js
@@ -5,8 +5,9 @@
       const scene = document.createElement('div');
       scene.className = 'fxcs-scene';
       scene.setAttribute('aria-hidden', 'true');
-      scene.innerHTML = '<div class="fxcs-map"><span>A</span><span>B</span>' +
-        '<i></i><i></i><i></i><i></i><b></b></div>' +
+      scene.innerHTML = '<div class="fxcs-teams"><div><b>CT</b><span><i></i><i></i><i></i><i></i><i></i></span><strong>4</strong></div>' +
+        '<div><b>T</b><span><i></i><i></i><i></i><i></i><i></i></span><strong>3</strong></div>' +
+        '<p class="fxcs-c4"><span>C4</span><code>7355608</code><em>ARMED</em></p></div>' +
         '<div class="fxcs-score"><b>12</b><span class="fxcs-clock">0:45</span><b>09</b></div>' +
         '<div class="fxcs-site">A</div><div class="fxcs-cross"><i></i><i></i></div>';
       hero.append(scene);
